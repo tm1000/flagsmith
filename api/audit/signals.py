305d849e8951d61ce0bca95b3c58e9a5cfa7d3ac
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2021-10-25 12:45:53 +0530

    fix: flake8 errors

--- api/audit/signals.py
+++ api/audit/signals.py
@@ -2,7 +2,6 @@ import logging
 
 import boto3
 from django.conf import settings
-from django.core.exceptions import ObjectDoesNotExist
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from flag_engine.environments.builders import build_environment_dict
